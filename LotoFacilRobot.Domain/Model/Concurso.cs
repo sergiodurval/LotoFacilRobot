7d5797d3ec8a175db3af632704442a94f158e6dd
SergioDurval <sergiodurval2@gmail.com>
2018-04-22 11:31:58 -0300

    Criando a camada de dominio

---
+++ LotoFacilRobot.Domain/Model/Concurso.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotoFacilRobot.Domain.Model
+{
+    public class Concurso
+    {
+        public int IdConcurso { get; set; }
+        public int NumeroConcurso { get; set; }
+        public DateTime DataResultado { get; set; }
+        public double PremioEstimado { get; set; }
+        public DateTime ProximoConcurso { get; set; }
+        public List<int> NumerosSorteados { get; set; }
+    }
+}
---
+++ LotoFacilRobot.Domain/Model/QuantidadeAcertos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotoFacilRobot.Domain.Model
+{
+    class QuantidadeAcertos
+    {
+        public int IdResultado { get; set; }
+        public int IdConcurso { get; set; }
+        public int QuinzeAcertos { get; set; }
+        public int QuatorzeAcertos { get; set; }
+        public int TrezeAcertos { get; set; }
+        public int DozeAcertos { get; set; }
+        public int OnzeAcertos { get; set; }
+    }
+}

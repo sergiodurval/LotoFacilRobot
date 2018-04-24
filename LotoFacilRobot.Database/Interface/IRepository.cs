979c7f9cfc5095fe9133c8b761f8e038de5ebec7
SergioDurval <sergiodurval2@gmail.com>
2018-04-23 23:35:26 -0300

    Criando a interface para o repositorio

---
+++ LotoFacilRobot.Database/Interface/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotoFacilRobot.Database.Interface
+{
+    interface IRepository<TEntity> : IDisposable where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById();
+        void Insert(TEntity entity);
+        void UpdateById(TEntity entity, int key);
+        void Delete(TEntity entity);
+        void DeleteById(TEntity entity, int key);
+    }
+}

03b7f11136bf03f407a76f2b06bafb44e180b4f7
SergioDurval <sergiodurval2@gmail.com>
2018-04-25 01:50:16 -0300

    Implementando a interface e add o appconfig

--- LotoFacilRobot.Database/Interface/IRepository.cs
+++ LotoFacilRobot.Database/Interface/IRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LotoFacilRobot.Database.Interface
 {
-    interface IRepository<TEntity> : IDisposable where TEntity : class
+    interface IRepository<TEntity> where TEntity : class
     {
         IEnumerable<TEntity> GetAll();
         TEntity GetById();
---
+++ LotoFacilRobot.Database/Repository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LotoFacilRobot.Database.Interface;
+namespace LotoFacilRobot.Database
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateById(T entity, int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(T entity, int key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

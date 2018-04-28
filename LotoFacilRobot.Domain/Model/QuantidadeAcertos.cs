dff67885ec919090ffeaee4b7ba2702de0bb82c2
SergioDurval <sergiodurval2@gmail.com>
2018-04-28 17:29:55 -0300

    Alteracoes nas models

--- LotoFacilRobot.Domain/Model/QuantidadeAcertos.cs
+++ LotoFacilRobot.Domain/Model/QuantidadeAcertos.cs
@@ -6,14 +6,19 @@ using System.Threading.Tasks;
 
 namespace LotoFacilRobot.Domain.Model
 {
-    class QuantidadeAcertos
+    public class QuantidadeAcertos
     {
         public int IdResultado { get; set; }
         public int IdConcurso { get; set; }
-        public int QuinzeAcertos { get; set; }
-        public int QuatorzeAcertos { get; set; }
-        public int TrezeAcertos { get; set; }
-        public int DozeAcertos { get; set; }
-        public int OnzeAcertos { get; set; }
+        public double QuinzeAcertos { get; set; }
+        public double QuatorzeAcertos { get; set; }
+        public double TrezeAcertos { get; set; }
+        public double DozeAcertos { get; set; }
+        public double OnzeAcertos { get; set; }
+        public double ValorPremioQuinzeAcertos { get; set; }
+        public double ValorPremioQuatorzeAcertos { get; set; }
+        public double ValorPremioTrezeAcertos { get; set; }
+        public double ValorPremioDozeAcertos { get; set; }
+        public double ValorPremioOnzeAcertos { get; set; }
     }
 }

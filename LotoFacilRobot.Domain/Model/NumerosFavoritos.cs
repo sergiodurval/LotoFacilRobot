0f801bdb0319dace59ef5a7e160c3b257999d5ab
SergioDurval <sergiodurval2@gmail.com>
2018-05-03 00:35:40 -0300

    Adicionando novo model

---
+++ LotoFacilRobot.Domain/Model/NumerosFavoritos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotoFacilRobot.Domain.Model
+{
+    public class NumerosFavoritos
+    {
+        public int Id { get; set; }
+        public List<int> NumerosFavoritos { get; set; }
+        public bool Ativo { get; set; }
+    }
+}

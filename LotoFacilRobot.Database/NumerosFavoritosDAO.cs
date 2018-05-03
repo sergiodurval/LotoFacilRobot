3b1fc80bb68d8df9b942fc5df87ae1d7d62a7b02
SergioDurval <sergiodurval2@gmail.com>
2018-05-03 20:51:48 -0300

    Temino da classe de acesso a dados numeros favoritos

--- LotoFacilRobot.Database/NumerosFavoritosDAO.cs
+++ LotoFacilRobot.Database/NumerosFavoritosDAO.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using LotoFacilRobot.Domain.Model;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LotoFacilRobot.Database
 {
-    class NumerosFavoritosDAO
+    public class NumerosFavoritosDAO
     {
+        public string strConnection = ConfigurationManager.ConnectionStrings["LotoFacilDBConnection"].ConnectionString;
+        public SqlConnection conn;
+
+        public void InsertNumerosFavoritos(NumerosFavoritos numerosFavoritos)
+        {
+            try
+            {
+                using (conn = new SqlConnection(strConnection))
+                {
+                    SqlCommand command = new SqlCommand("PR_INSERT_NUMEROS_FAVORITOS",conn);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@NumerosFavoritos",string.Join("-",numerosFavoritos.ListaNumerosFavoritos));
+                    command.Parameters.AddWithValue("@Ativo", numerosFavoritos.Ativo);
+                    conn.Open();
+                    command.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                throw new Exception("Ocorreu o seguinte erro: " + ex.Message);
+            }
+        }
+
+        public DataTable GetAllNumerosFavoritos()
+        {
+            try
+            {
+                using (conn = new SqlConnection(strConnection))
+                {
+                    SqlCommand command = new SqlCommand("PR_GET_ALL_NUMEROS_FAVORITOS", conn);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(command);
+                    da.Fill(dt);
+                    da.Dispose();
+                    return dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                throw new Exception("Ocorreu o seguinte erro: " + ex.Message);
+            }
+        }
     }
 }
